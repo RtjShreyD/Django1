d9ec9a63511313dab28181526daee32fb0998cb1
Shreyansh <shreyanshece1041@gmail.com>
2019-08-28 18:30:02 +0530

    Added static files to template

--- hello/urls.py
+++ hello/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from hello import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [path("hello/<name>", views.hello_there, name="hello_there"),path("", views.home, name=""), ]
+urlpatterns += staticfiles_urlpatterns()
