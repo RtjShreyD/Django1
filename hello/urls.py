274b9f485a5fa8506e14a086fbc38b6681585dad
Shreyansh <shreyanshece1041@gmail.com>
2019-08-28 19:49:23 +0530

    Created and Used Snippets to add pages

--- hello/urls.py
+++ hello/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from hello import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-urlpatterns = [path("hello/<name>", views.hello_there, name="hello_there"),path("", views.home, name=""), ]
+urlpatterns = [path("hello/<name>", views.hello_there, name="hello_there"),
+               path("home/", views.home, name="home"),
+               path("about/", views.about, name="about"),
+               path("contact/", views.contact, name="contact"),
+                ]
 urlpatterns += staticfiles_urlpatterns()
